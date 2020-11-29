45d6c9a92373b54be71c0a0f8a4f2eb04e7a80f7
Pete <pavlovskipetko@abv.bg>
2020-11-29 22:10:20 +0200

    user profile

---
+++ lib/views/userProfile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quizapp/helper/constants.dart';
+import 'package:quizapp/views/userMenu.dart';
+import 'package:quizapp/widgets/widgets.dart';
+
+class UserProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: appBar(context),
+          actions: [
+            UserMenuActions(),
+          ],
+          centerTitle: true,
+          backgroundColor: MAIN_COLOR,
+          elevation: 0,
+          brightness: Brightness.light),
+      body: Container(),
+    );
+  }
+}
